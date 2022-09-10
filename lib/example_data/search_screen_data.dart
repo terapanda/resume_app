af9136f60f6e9651504e53c2a1904efaf836d014
Yuki Watanabe <hinatabokco@gmail.com>
2022-09-10 18:36:55 +0900

    検索結果画面　雑に作成

---
+++ lib/example_data/search_screen_data.dart
@@ -0,0 +1,49 @@
+import 'package:resume_app/model/person.dart';
+
+List<Person> exampleList = [
+  const Person(
+    id: "1",
+    name: "渡部　悠貴",
+    ruby: "わたなべ　ゆうき",
+    sex: "1",
+    age: "29",
+    station: "地下鉄御堂筋線　江坂",
+    image: "assets/img/earth.png",
+  ),
+  const Person(
+    id: "2",
+    name: "谷内　紳助",
+    ruby: "たにうち　しんすけ",
+    sex: "1",
+    age: "34",
+    station: "JRおおさか東線　高井田中央",
+    image: "assets/img/earth.png",
+  ),
+  const Person(
+    id: "3",
+    name: "保木本　大貴",
+    ruby: "ほきもと　ひろき",
+    sex: "1",
+    age: "35",
+    station: "JRおおさか東線　鴫野",
+    image: "assets/img/earth.png",
+  ),
+  const Person(
+    id: "4",
+    name: "ボス",
+    ruby: "ぼす",
+    sex: "2",
+    age: "34",
+    station: "阪神電鉄　野田",
+    image: "assets/img/earth.png",
+  ),
+  const Person(
+    id: "5",
+    name: "平　凌侑",
+    ruby: "たいら　りょうすけ",
+    sex: "1",
+    age: "29",
+    station: "地下鉄御堂筋線　東三国",
+    image: "assets/img/earth.png",
+  ),
+];
