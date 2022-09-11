e3d96e153acbd0bfbb3c3448d25bacd47f2a5aab
Yuki Watanabe <hinatabokco@gmail.com>
2022-09-11 10:54:38 +0900

    検索結果画面　経歴情報をカードで表示

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,37 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const MaterialColor white = MaterialColor(
+    0xFFFFFFFF,
+    <int, Color>{
+      50: Color(0xFFFFFFFF),
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFFFFF),
+      300: Color(0xFFFFFFFF),
+      400: Color(0xFFFFFFFF),
+      500: Color(0xFFFFFFFF),
+      600: Color(0xFFFFFFFF),
+      700: Color(0xFFFFFFFF),
+      800: Color(0xFFFFFFFF),
+      900: Color(0xFFFFFFFF),
+    },
+  );
+
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TopScreen(),
+      home: const TopScreen(),
     );
   }
 }
